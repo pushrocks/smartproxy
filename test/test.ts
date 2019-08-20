8b5ba84215b5f6953664ef81753c7342f062c0da
Phil Kunz <gitlab@philkunz.com>
2019-08-20 17:50:17 +0200

    fix(core): update

---
+++ test/test.ts
@@ -0,0 +1,8 @@
+import { expect, tap } from '@pushrocks/tapbundle';
+import * as smartproxy from '../ts/index'
+
+tap.test('first test', async () => {
+  console.log(smartproxy.standardExport)
+})
+
+tap.start()
---
+++ ts/index.ts
@@ -0,0 +1,3 @@
+import * as plugins from './smartproxy.plugins';
+
+export let standardExport = 'Hi there! :) This is an exported string';
